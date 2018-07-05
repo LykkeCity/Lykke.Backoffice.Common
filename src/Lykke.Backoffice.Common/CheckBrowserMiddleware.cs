2dd3df2ccac1f945ed1c1ddcdbc4178e525750ed
olegtrofimenko <oleg.trofimenko@lykke.com>
2018-07-05 18:20:25 +0500

    initial commit

---
+++ src/Lykke.Backoffice.Common/CheckBrowserMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Lykke.Backoffice.Common.Extensions.BrowserExtension;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lykke.Backoffice.Common
+{
+    /// <summary>
+    /// Check minimal supported browser version for backoffice
+    /// </summary>
+    public class CheckBrowserMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private IDictionary<string, int> BrowserMajorVarsionMap = new Dictionary<string, int>
+        {
+            {"Chrome", 25},
+            {"Firefox", 23},
+            {"Safari", 7},
+            {"Edge", 13},
+        };
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="next"></param>
+        public CheckBrowserMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        /// <summary>
+        /// Invoke method
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public Task Invoke(HttpContext context)
+        {
+            var useragentHeader = context.Request.Headers["User-Agent"];
+            var useragent = new UserAgent(useragentHeader);
+            var supportedBrowser = CheckBrowserMajorVersion(useragent.Browser.Name, useragent.Browser.Major);
+            context.Response.StatusCode = 200;
+            if (!supportedBrowser)
+            {
+                context.Response.StatusCode = 403;
+                context.Response.WriteAsync("<html><div>Forbidden</div></html>");
+            }
+            return _next(context);
+        }
+        private bool CheckBrowserMajorVersion(string name, string useragentVersion)
+        {
+            if (string.IsNullOrWhiteSpace(name) || !BrowserMajorVarsionMap.TryGetValue(name, out var version))
+                return false;
+            return Convert.ToInt32(useragentVersion) > version;
+        }
+    }
+}
---
+++ src/Lykke.Backoffice.Common/Extensions/BrowserExtension/UserAgent.cs
@@ -0,0 +1,33 @@
+﻿namespace Lykke.Backoffice.Common.Extensions.BrowserExtension
+{
+    /// <summary>
+    /// User agent
+    /// </summary>
+    public class UserAgent
+    {
+        private string _userAgent;
+        private ClientBrowser _browser;
+        /// <summary>
+        /// Client browser
+        /// </summary>
+        public ClientBrowser Browser
+        {
+            get
+            {
+                if (_browser == null)
+                {
+                    _browser = new ClientBrowser(_userAgent);
+                }
+                return _browser;
+            }
+        }
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="userAgent"></param>
+        public UserAgent(string userAgent)
+        {
+            _userAgent = userAgent;
+        }
+    }
+}
