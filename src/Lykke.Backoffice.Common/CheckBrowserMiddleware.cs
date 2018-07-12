79245087159d1f2e51ca8c69765c1a357ef0f45a
olegtrofimenko <oleg.trofimenko@lykke.com>
2018-07-12 18:40:43 +0500

    fixs

--- src/Lykke.Backoffice.Common/CheckBrowserMiddleware.cs
+++ src/Lykke.Backoffice.Common/CheckBrowserMiddleware.cs
@@ -17,8 +17,8 @@ namespace Lykke.Backoffice.Common
         private readonly IEnumerable<Browser> _browsers;
         private readonly IEnumerable<string> _skipUrls;
 
-        private const string templateMessage = "<html><div>Forbidden, because your browser does not meet safety requirements. Following browsers are allowed:</div><div>{0}</div></html>";
-        private const string templateBrowser = "<div>{0} min version: '{1}', max version: '{2}'</div>";
+        private const string TemplateMessage = "<html><div>Forbidden, because your browser does not meet safety requirements. Following browsers are allowed:</div><div>{0}</div></html>";
+        private const string TemplateBrowser = "<div>{0} min version: '{1}', max version: '{2}'</div>";
         /// <summary>
         /// Ctor
         /// </summary>
@@ -67,8 +67,8 @@ namespace Lykke.Backoffice.Common
                 context.Response.StatusCode = 403;
                 var sb = new StringBuilder();
                 foreach (var browser in _browsers)
-                    sb.AppendLine(string.Format(templateBrowser, browser.Name, browser.MinMajorVersion, browser.MaxMajorVersion));
-                await context.Response.WriteAsync(string.Format(templateMessage, sb.ToString()));
+                    sb.AppendFormat(TemplateBrowser, browser.Name, browser.MinMajorVersion, browser.MaxMajorVersion);
+                await context.Response.WriteAsync(string.Format(TemplateMessage, sb.ToString()));
             }
             await _next(context);
         }
